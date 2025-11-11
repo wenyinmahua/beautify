3b4f210133f2d995f8df54a3009de0e0704d5aca
ma <501847822@qq.com>
2025-11-11 22:41:47 +0800

    first commit

---
+++ src/main/java/com/design/beautify/BeautifyApplication.java
@@ -0,0 +1,13 @@
+package com.design.besutify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesutifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesutifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/design/beautify/BesutifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.besutify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BesutifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
